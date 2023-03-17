a904f97f68a6b4d5414a594ee89f9f6d3de0ac95
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2023-03-17 17:41:13 +0900

    added vars to inspect with gdb to hello_serial

--- hello_world/serial/hello_serial.c
+++ hello_world/serial/hello_serial.c
@@ -6,8 +6,14 @@
 
 #include <stdio.h>
 #include "pico/stdlib.h"
+#define a 10
+#define b 20
+
 
 int main() {
+    const int c = 30;
+    const float d = 40.4;
+    const char mystr[] = "HelloWorld";
     stdio_init_all();
     while (true) {
         printf("Hello, world!\n");
