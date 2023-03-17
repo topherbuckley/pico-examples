6f32d21058b23557cdcefa48037c3d5cc097c43a
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2023-03-17 17:40:37 +0900

    i2c bus scan on I2C0

--- i2c/bus_scan/bus_scan.c
+++ i2c/bus_scan/bus_scan.c
@@ -25,6 +25,9 @@
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
 
+#define ROBOT_I2C_SDA_PIN 0
+#define ROBOT_I2C_SCL_PIN 1
+
 // I2C reserves some addresses for special purposes. We exclude these from the scan.
 // These are any addresses of the form 000 0xxx or 111 1xxx
 bool reserved_addr(uint8_t addr) {
@@ -34,18 +37,13 @@ bool reserved_addr(uint8_t addr) {
 int main() {
     // Enable UART so we can print status output
     stdio_init_all();
-#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
-#warning i2c/bus_scan example requires a board with I2C pins
-    puts("Default I2C pins were not defined");
-#else
-    // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
     i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    gpio_set_function(ROBOT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(ROBOT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(ROBOT_I2C_SDA_PIN);
+    gpio_pull_up(ROBOT_I2C_SCL_PIN);
     // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+    bi_decl(bi_2pins_with_func(ROBOT_I2C_SDA_PIN, ROBOT_I2C_SCL_PIN, GPIO_FUNC_I2C));
 
     printf("\nI2C Bus Scan\n");
     printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
